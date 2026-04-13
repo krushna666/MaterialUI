ffa20fb94dc62d2d62a2451e22503b7739a9c670
krushna666 <krushnadeore666@gmail.com>
2026-04-13 18:34:59 +0530

    edit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatProgressBarModule,MatButtonModule,MatButtonToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
